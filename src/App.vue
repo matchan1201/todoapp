ae1208db2e477da5b340466d8dd3b3174a00dcee
matchan1201 <bumpakano.1@gmail.com>
2020-12-29 19:19:49 +0900

    todoの追加と削除機能の追加

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
     <ul>
       <li v-for="todo in todos" :key='todo'> <!--v-forを使う際は、keyを指定する -->
         <span>{{ todo.text }}</span>
+        <span @click="deleteTodo(todo)">[x]
+        </span>
       </li>
     </ul>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo">
+      <input type="submit" value="Add">
+    </form>
   </div>
 </template>
 
@@ -17,7 +23,18 @@ export default {
         { id: 1, text: 'dataを表示する' },
         { id: 2, text: 'ディレクティブを知る' },
       ],
+      newTodo: ''
     }
+  },
+  methods: {
+    addTodo: function() {
+      const newId = Math.max.apply(null, this.todos.map(t => t.id)) + 1;
+      this.todos.push({ id: newId, text: this.newTodo});
+      this.newTodo = '';
+    },
+    deleteTodo: function(todo) {
+      this.todos = this.todos.filter(item => item !== todo);
+    },
   }
 }
 </script>
