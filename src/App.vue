5b185c6359867c95d12b406190747e5ee016a856
matchan1201 <bumpakano.1@gmail.com>
2020-12-29 21:17:07 +0900

    todo完了状態を追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
+    <h1>
+      My todos
+       <span class="info">({{ remaining.length}}/{{ todos.length }})</span>
+    </h1>
     <ul>
-      <li v-for="todo in todos" :key='todo'> <!--v-forを使う際は、keyを指定する -->
-        <span>{{ todo.text }}</span>
-        <span @click="deleteTodo(todo)">[x]
-        </span>
-      </li>
+      <ToDoItem v-for="(todo,index) in todos"
+                :todo="todo"
+                @delete="deleteTodo" :key="index"/>
     </ul>
     <form @submit.prevent="addTodo">
       <input type="text" v-model="newTodo">
@@ -15,13 +17,17 @@
 </template>
 
 <script>
+import ToDoItem from './components/ToDoItem.vue'
 export default {
   name: 'app',
+  components: {
+    ToDoItem
+  },
   data() {
     return {
       todos: [
-        { id: 1, text: 'dataを表示する' },
-        { id: 2, text: 'ディレクティブを知る' },
+        { id: 1, text: 'dataを表示する', isDone: false },
+        { id: 2, text: 'ディレクティブを知る', isDone: false },
       ],
       newTodo: ''
     }
@@ -29,12 +35,19 @@ export default {
   methods: {
     addTodo: function() {
       const newId = Math.max.apply(null, this.todos.map(t => t.id)) + 1;
-      this.todos.push({ id: newId, text: this.newTodo});
+      this.todos.push({ id: newId, text: this.newTodo, isDone: false});
       this.newTodo = '';
     },
     deleteTodo: function(todo) {
       this.todos = this.todos.filter(item => item !== todo);
     },
+  },
+  computed: {
+    remaining: function() {
+      return this.todos.filter(function(todo) {
+        return !todo.isDone;
+      });
+    }
   }
 }
 </script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <input type="checkbox" v-model="todo.isDone">
+    <span>{{ todo.text }}</span>
+    <span @click="deleteItem(todo)">[x]</span>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: "to-do-item",
+    props: ['todo'],
+    methods: {
+      deleteItem(todo) {
+        this.$emit('delete', todo)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
